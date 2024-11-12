fb8cdd681fcbbadfcda92c0c182e12a0d643cef5
Nopnapat Norasri <nopnapatn@gmail.com>
2024-11-12 20:30:36 +0700

    build(*): setup shadcn ui component && dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+
+import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css"
 
 const geistSans = localFont({
@@ -21,14 +23,21 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
