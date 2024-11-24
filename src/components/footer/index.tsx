34306d9cbb6cdd74c7c518d9610f792adb06bd1e
Nopnapat Norasri <nopnapatn@gmail.com>
2024-11-24 23:34:50 +0700

    fix(*): run lint:fix

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+
 import {
   CONVERSATION_ROUTE,
   FAVORITE_ROUTE,
